3d595948b12d7566631399b17082a729d47a0727
dgtlmoon <dgtlmoon@gmail.com>
2024-07-30 17:05:05 +0200

    0.02

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="changedetectionio_amazon_restock_price_scraper",
-    version="0.01",
+    version="0.02",
     packages=["cdio_amazon_restock_price_scraper"],
+    long_description="A small scraper plugin to get the price data from Amazon pages which can be used to improve the 'restock and price detection' mode in changedetection.io",
     entry_points={
         "changedetectionio.restock_price_scraper": [
             "cdio_amazon_restock_price_scraper = cdio_amazon_restock_price_scraper.plugin:restock_price_scraper",
